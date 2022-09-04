2a7068b1e4ac3492ae123223f5e9e6c8da601abb
badobedoor <ansbedoor@gmail.com>
2022-09-04 15:21:14 +0200

    add animation

--- lib/presentation/widgets/container_with_logo_background_image.dart
+++ lib/presentation/widgets/container_with_logo_background_image.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:who_win_million/presentation/widgets/playLogoAnimation.dart';
 
 class ContainerWithLogoBackgroundImage extends StatelessWidget {
   final Widget child;
+  final String pageName;
   ContainerWithLogoBackgroundImage({
     Key? key,
     required this.child,
+    required this.pageName,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // 640×360 px
-        height: 360.h,
-        width: 640.w,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/bgLogo.png"), fit: BoxFit.fill),
-        ),
-        child: child);
+    return Stack(
+      children: [
+        Container(
+            // 640×360 px
+            height: 360.h,
+            width: 640.w,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: pageName == 'homeScreen'
+                      ? AssetImage("assets/images/bgAsset.png")
+                      : AssetImage("assets/images/bgLogo.png"),
+                  fit: BoxFit.fill),
+            ),
+            child: child),
+        if (pageName == 'homeScreen')
+          Positioned(top: 14.h, left: 14.h, child: PlayLogoAnimation()),
+
+        //  Container(
+        // // 640×360 px
+        // height: 360.h,
+        // width: 640.w,
+        // decoration: const BoxDecoration(
+        //   image: DecorationImage(
+        //       image: AssetImage("assets/images/bgAsset.png"), fit: BoxFit.fill),
+        // ),
+        // child: child),
+      ],
+    );
   }
 }
